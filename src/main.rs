6598b3cf2a70bb81aaa4e303ef7f11b0a9b80b3e
Luciano Simoni <71110718+lucianosimoni@users.noreply.github.com>
2024-02-29 10:06:59 +0000

    did with docs and small knowledge

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,36 @@
+use rand::Rng;
 use std::io;
 use std::cmp::Ordering;
-use rand::Rng;
 
 fn main() {
-    println!("🟢 Guess the number 🟢");
-
-    let secret: u32 = gen_rand_num(false);
-    let mut tries_count: u8 = 0;
-
+    println!("☀️📦 Guessing Game!");
+    
+    // generate rand code
+    let secret: u32 = rand::thread_rng().gen_range(1..100);
+    
+    println!( "📦 secret memory addr: {:p}", &secret);
+    
     loop {
-        println!("\n⏱️ Num of Tries: {tries_count}");
-        let guess: u32 = take_guess();
-        let correct: bool = cmp_guess(guess, secret);
+        // get input
+        println!("Enter a number:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Input invalid.");
         
-        if correct {
-             break
-        } else {
-            tries_count = tries_count + 1;
-        }
+        // save as u32
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("🔴 must be a positive num.");
+                continue
+                },
+        };
         
+        // compare it
+        match input.cmp(&secret)  {
+            Ordering::Less => { println!("🔻 {input} is too low."); continue },
+            Ordering::Equal => { println!("🟢 You got it! 💐"); break },
+            Ordering::Greater => { println!("🔺 {input} is too high."); continue },
+        };
     }
-}
-
-fn gen_rand_num(print_it: bool) -> u32 {
-    let secret_num = rand::thread_rng().gen_range(1..=100);
-
-    if print_it {
-        println!("🔑 The secret number is: {secret_num}");
-    }
-
-    secret_num
-}
-
-fn take_guess() -> u32 {
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number.");
-
-    println!("You guessed: {guess}");
-
-    guess
-}
-
-fn cmp_guess(guess: u32, secret: u32) -> bool {
-    // Comparison
-    match guess.cmp(&secret) {
-        Ordering::Less => {
-            println!("🔴👎 - It is too smalll");
-            false
-        },
-        Ordering::Greater => {
-            println!("🔴👍 - It is too big");
-            false
-        },
-        Ordering::Equal => {
-            println!("Correct! 🎊");
-            true
-        }
-    }
+    
 }
